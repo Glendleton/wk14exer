b3705ba70a044b93e401afa4de594344ac32a404
Glendleton <gpendlet92@gmail.com>
2018-08-26 14:39:57 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// discussion questions
+//1) application program interface
+// allows you to grab data from a third party
+// 2)documentation associated with api
+// if the database is poorly documented, then the api won't be as effective
+// 3) $.ajax
+// known as "ajax call"
+// 4) type: "GET"
+// 5) dataType: "json"
+// 6) java script object notation
+
+// first step 4) Visit W3Schools Google Maps Basics (link)
+
+// step 7)Create a function to add the map, with one variable for map properties. Update the location to your favorite place on Earth.
+
+// step8)Using the google.maps.event.addDomListener method, select the window and when it loads, run the function to add the map.
+
+// step9)Add the map to the screen using the google.maps.Map method.
+
+// step10-11)In W3Schools: Move to the next section Maps Overlays. Next, Create a marker and set the position to mapProp.center. Add the marker to the map using the Google setMap method.
+
+// step12-13)In W3Schools: Move to the next section Maps Events. Next,Using the google.maps.event.addListener method, add an event so when you click on the marker it zooms in on the map marker.
+
+// step14-15)In W3Schools: Move to the next section Maps Controls. Next,Disable the default controls on the map.
